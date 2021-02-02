3a1301a49a2cd9e541e421c0ee2b673a3cedcaa9
Aashim Bajracharya <ashimbazracharya@gmail.com>
2021-02-02 12:57:58 +0545

    Get concept of context for view

--- leads/views.py
+++ leads/views.py
@@ -4,5 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
     
+    # Add context in the view to render
+    context = {
+        "name": "Sanu",
+        "age": 40 
+    }
+
     # Render view with html template
-    return render(request, "second_page.html")
+    return render(request, "second_page.html", context)
