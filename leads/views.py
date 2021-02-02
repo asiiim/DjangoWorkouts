1baf0efe3f388ce2ea9e985442001511d396faa9
Aashim Bajracharya <ashimbazracharya@gmail.com>
2021-02-02 12:51:11 +0545

    Render view with template outside app

--- leads/views.py
+++ leads/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 def home_page(request):
     
     # Render view with html template
-    return render(request, "leads/home_page.html")
+    return render(request, "second_page.html")
